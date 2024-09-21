001ae0233435e14506c20ea9df4e492e08123c1c
Javier Luque <javier.lr9@gmail.com>
2024-09-21 15:31:02 +0200

    modified makefile

--- Makefile
+++ Makefile
@@ -8,29 +8,30 @@ all: dist/extension.js
 node_modules: package.json
 	npm install
 
-dist/extension.js dist/prefs.js: node_modules
-	tsc
-
 # schemas/gschemas.compiled: src/schemas/org.gnome.shell.extensions.$(NAME).gschema.xml
 # 	glib-compile-schemas schemas
 
-$(NAME).zip: dist/extension.js dist/prefs.js #schemas/gschemas.compiled
+build: #schemas/gschemas.compiled
+	tsc
 	# @cp -r src/schemas dist/
 	@cp metadata.json dist/
 	@cp -r src/icons/ dist/
+
+zip: node_modules build
 	@(cd dist && zip ../$(NAME).zip -9r .)
 
-pack: $(NAME).zip
+pack: zip
 
-install: $(NAME).zip
-	@touch ~/.local/share/gnome-shell/extensions/$(NAME)@$(DOMAIN)
+install_dev: build
 	@rm -rf ~/.local/share/gnome-shell/extensions/$(NAME)@$(DOMAIN)
 	@mv dist ~/.local/share/gnome-shell/extensions/$(NAME)@$(DOMAIN)
+
+install: node_modules install_dev
 
 run:
 	dbus-run-session -- gnome-shell --nested --wayland
 
-dev: install run
+dev: install_dev run
 
 clean:
 	@rm -rf dist node_modules $(NAME).zip
--- src/lib/timer.ts
+++ src/lib/timer.ts
@@ -1,57 +1,36 @@
 import GLib from "gi://GLib";
-import { FitBreaksIndicator } from "./indicator.js";
-import { notify } from "./notification.js";
+import GObject from "gi://GObject";
 
-export class Timer {
-  private indicator: FitBreaksIndicator;
-  private timerDuration = 1800; // 30 min
-  private interval?: GLib.Source;
-  private exercises = ["Stretch neck"];
+export const Timer = GObject.registerClass(
+  {
+    Signals: {
+      tic: {},
+      finish: {},
+    },
+  },
+  class Timer extends GObject.Object {
+    private timerDuration = 1800; // 30 min
+    private interval?: GLib.Source;
 
-  constructor(indicator: FitBreaksIndicator) {
-    this.indicator = indicator;
-  }
-
-  startTimer() {
-    let secondsLeft = this.timerDuration;
-    this.printTimer(secondsLeft);
-    this.interval = setInterval(() => {
-      secondsLeft -= 1;
-      this.printTimer(secondsLeft);
-      if (secondsLeft <= 0) this.restart();
-    }, 1000);
-  }
-
-  private formatTime(seconds: number) {
-    const hours = Math.floor(seconds / 3600)
-      .toString()
-      .padStart(2, "0");
-    const min = Math.floor((seconds % 3600) / 60)
-      .toString()
-      .padStart(2, "0");
-    const sec = Math.floor(seconds % 60)
-      .toString()
-      .padStart(2, "0");
-
-    if (hours !== "00") {
-      return hours + ":" + min + ":" + sec;
-    } else {
-      return min + ":" + sec;
+    startTimer() {
+      let secondsLeft = this.timerDuration;
+      this.emit("tic", secondsLeft);
+      this.interval = setInterval(() => {
+        secondsLeft -= 1;
+        this.emit("tic", secondsLeft);
+        if (secondsLeft <= 0) this.restart();
+      }, 1000);
     }
-  }
 
-  private printTimer(seconds: number) {
-    if (!this.indicator.timerLabel) return;
-    this.indicator.timerLabel.text = this.formatTime(seconds);
-  }
+    private restart() {
+      this.removeTimer();
+      this.emit("finish");
+    }
 
-  private restart() {
-    this.removeTimer();
-    this.exercises[0] &&
-      notify(this.exercises[0]).finally(() => this.startTimer());
-  }
+    removeTimer() {
+      if (this.interval) clearInterval(this.interval);
+    }
+  },
+);
 
-  removeTimer() {
-    if (this.interval) clearInterval(this.interval);
-  }
-}
+export type Timer = InstanceType<typeof Timer>;
