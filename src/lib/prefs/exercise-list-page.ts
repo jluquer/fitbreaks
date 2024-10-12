09782800a092fbddd0b2f91e6befcbc9acd31c45
Javier Luque <javier.lr9@gmail.com>
2024-10-12 02:15:46 +0200

    add exercise

--- src/lib/prefs/exercise-list-page.ts
+++ src/lib/prefs/exercise-list-page.ts
@@ -1,6 +1,7 @@
 import GObject from "gi://GObject";
 import Adw from "gi://Adw";
 import Gio from "gi://Gio";
+import Gtk from "gi://Gtk";
 
 import { gettext as _ } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 import { schemas } from "../constants.js";
@@ -13,6 +14,8 @@ export const ExerciseListPage = GObject.registerClass(
   class ExerciseListPage extends Adw.PreferencesPage {
     exercisesGroup: Adw.PreferencesGroup;
     settings: Gio.Settings;
+    exercises: string[];
+    exercisesRows: Adw.ActionRow[] = [];
 
     constructor(props: Props) {
       super({
@@ -23,41 +26,85 @@ export const ExerciseListPage = GObject.registerClass(
       this.exercisesGroup = new Adw.PreferencesGroup({
         title: _("Exercise List"),
       });
+      this.exercises = this.getExercisesFromSettings();
       this.refreshExerciseList();
+      this.displayAddExercise();
       this.add(this.exercisesGroup);
-      this.settings.connect("changed::exercises", () =>
-        this.refreshExerciseList(),
-      );
-    }
-
-    private getExercises() {
-      return this.settings.get_strv(schemas.exercises);
-    }
-
-    // private setExercises(exercises: string[]) {
-    //   this.settings.set_strv("exercises", exercises);
-    // }
-
-    private refreshExerciseList() {
-      this.cleanExerciseGroup();
-      console.log("refreshing exercises");
-      this.getExercises()?.forEach((exercise) => {
-        this.exercisesGroup.add(
-          new Adw.ActionRow({
-            name: exercise,
-            title: exercise,
-            iconName: "org.gnome.Settings-trash-file-history-symbolic",
-          }),
-        );
+      this.settings.connect("changed::exercises", () => {
+        console.log("exercises settings changed");
+        this.refreshExerciseList();
       });
     }
 
-    private cleanExerciseGroup() {
-      let child = this.exercisesGroup.get_first_child();
-      while (child) {
-        this.exercisesGroup.remove(child);
-        child = child.get_next_sibling();
+    private displayAddExercise() {
+      const addExerciseGroup = new Adw.PreferencesGroup();
+      const box = new Gtk.Box({
+        orientation: Gtk.Orientation.HORIZONTAL,
+        cssClasses: ["linked", "round"],
+        hexpand: true,
+        vexpand: true,
+      });
+
+      const textInput = new Gtk.Entry({
+        placeholderText: _("New exercise"),
+      });
+      textInput.connect("activate", () => this.addNewExercise(textInput));
+
+      const addButton = new Gtk.Button({
+        child: new Adw.ButtonContent({
+          iconName: "list-add-symbolic",
+        }),
+      });
+      addButton.connect("clicked", () => this.addNewExercise(textInput));
+
+      box.append(textInput);
+      box.append(addButton);
+
+      addExerciseGroup.add(box);
+      this.add(addExerciseGroup);
+    }
+    private addNewExercise(entry: Gtk.Entry) {
+      console.log(`adding new exercise`);
+      console.log(`adding exercise: ${entry.text}`);
+      const newExercise = entry.text.trim();
+      if (newExercise.length > 0) {
+        const exercises = this.exercises ?? [];
+        exercises.push(newExercise);
+        this.setExercises(exercises);
+        entry.set_text("");
       }
+    }
+    // private removeExercise(exercise: string) {
+    //   const exercises = this.exercises.filter((e) => e !== exercise);
+    //   this.setExercises(exercises);
+    // }
+    private getExercisesFromSettings() {
+      return this.settings.get_strv(schemas.exercises);
+    }
+
+    private setExercises(exercises: string[]) {
+      this.exercises = exercises;
+      this.settings.set_strv("exercises", exercises);
+    }
+
+    private refreshExerciseList() {
+      this.cleanExerciseGroup();
+      this.exercises.forEach(this.addExerciseRow.bind(this));
+    }
+
+    private addExerciseRow(exercise: string) {
+      const actionRow = new Adw.ActionRow({
+        name: exercise,
+        title: exercise,
+        iconName: "org.gnome.Settings-trash-file-history-symbolic",
+      });
+      this.exercisesRows.push(actionRow);
+      this.exercisesGroup.add(actionRow);
+    }
+
+    private cleanExerciseGroup() {
+      this.exercisesRows.forEach((row) => this.exercisesGroup.remove(row));
+      this.exercisesRows = [];
     }
   },
 );
